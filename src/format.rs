a3ea8a6e283df355850f729c8d5a0c00029f3112
Andreas Maier <andreas.martin.maier@desy.de>
2023-08-24 15:06:44 +0200

    added missing file

---
+++ src/format.rs
@@ -0,0 +1,12 @@
+use clap::ValueEnum;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, ValueEnum)]
+#[clap(rename_all = "lower")]
+pub(crate) enum Format {
+    #[cfg(feature = "lhef")]
+    Lhef,
+    #[cfg(feature = "hepmc2")]
+    HepMC2,
+    #[cfg(feature = "ntuple")]
+    NTuple,
+}
